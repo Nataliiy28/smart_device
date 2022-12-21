258b2496a50ed78ba4fb3a019a99b2e67377a411
Nataliy <nk.12k@yandex.ru>
2022-12-21 23:35:04 +0300

    Подключает маску

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import { iosVhFix } from './utils/ios-vh-fix';
 import { initModals } from './modules/modals/init-modals';
 import './modules/modals/button-hidden';
-
+import './modules/modals/phone-mask';
 'use strict';
 
 const ButtonConsalt = document.querySelector('.page-header__button-consultation');
